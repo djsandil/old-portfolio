577c7a4f546046c36c038f794962dfbc68e870f5
Diana Sandil <dianajsandil@gmail.com>
2017-12-05 11:18:45 -0800

    Uploaded/Updated Final Portfolio

--- js/script.js
+++ js/script.js
@@ -11,3 +11,11 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
